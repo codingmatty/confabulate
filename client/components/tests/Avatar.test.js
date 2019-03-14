e671aadf0c9f6a051a3f47a988b1f22124e3e296
Matt Jacobs <hello@mattjdev.com>
2019-03-13 22:05:46 -0500

    Add infrastructure for testing components

---
+++ client/components/tests/Avatar.test.js
@@ -0,0 +1,19 @@
+import { render, cleanup } from 'react-testing-library';
+import md5 from 'md5-o-matic';
+import Avatar from '../Avatar';
+
+describe('Avatar', () => {
+  afterEach(cleanup);
+
+  it('matches default snapshot', () => {
+    const { container } = render(<Avatar email="test@email.com" />);
+    expect(container).toMatchSnapshot();
+  });
+
+  it('includes an img with gravatar src', () => {
+    const { container } = render(<Avatar email="test@email.com" />);
+    const hash = md5('test@email.com');
+    const expectedSrc = `https://s.gravatar.com/avatar/${hash}?s=80&d=mp`;
+    expect(container.querySelector('img')).toHaveAttribute('src', expectedSrc);
+  });
+});
