24ae6235c14b26da9c9c47a4d348ae4b732888ba
Matt Jacobs <hello@mattjdev.com>
2019-03-15 22:33:58 -0500

    Update Contact page display

--- client/components/Contact.jsx
+++ client/components/Contact.jsx
@@ -1,8 +1,12 @@
+import styled from 'styled-components';
 import { useState } from 'react';
 import PropTypes from 'prop-types';
 import { gql } from 'apollo-boost';
 import { useQuery } from 'react-apollo-hooks';
 import ContactForm from './ContactForm';
+import Avatar from './Avatar';
+import FavoriteContact from './FavoriteContact';
+import Icon from './Icon';
 
 const QUERY_CONTACT = gql`
   query QUERY_CONTACT($id: ID!) {
@@ -17,6 +21,40 @@ const QUERY_CONTACT = gql`
   }
 `;
 
+const ContactCard = styled.div`
+  position: relative;
+  padding: calc(60px + 1rem) 1rem 1rem 1rem;
+  margin-top: 60px;
+  border: 1px solid #888;
+  border-radius: 4px;
+  box-shadow: 2px 2px 4px -2px #333;
+`;
+const StyledContactAvatar = styled(Avatar)`
+  position: absolute;
+  margin: 0 auto;
+  top: -60px;
+  display: block;
+  left: 0;
+  right: 0;
+  box-shadow: 0 0 6px 1px #333;
+`;
+const ContactName = styled.h1`
+  margin-top: 0;
+  text-align: center;
+`;
+const StyledFavoriteContact = styled(FavoriteContact)`
+  position: absolute;
+  top: 0.5rem;
+  right: 0.5rem;
+`;
+const EditButton = styled.button`
+  background: none;
+  border: none;
+  position: absolute;
+  top: 0.5rem;
+  left: 0.5rem;
+`;
+
 export default function Contact({ id }) {
   const { data, loading, error } = useQuery(QUERY_CONTACT, {
     variables: { id }
@@ -24,31 +62,49 @@ export default function Contact({ id }) {
   const [editing, setEditing] = useState(false);
   const [contact, setContact] = useState(null);
 
-  if (loading) {
+  if (!loading && !contact) {
+    // sync the contact to state once it's queried
+    setContact(data.contact);
+  }
+  if (loading || !contact) {
     return <div>Loading...</div>;
   } else if (error) {
     return <div>Error! {error.message}</div>;
-  } else if (!contact) {
-    setContact(data.contact);
   }
 
   return (
-    <div>
+    <>
       {editing ? (
-        <ContactForm
-          contact={contact}
-          onSubmit={(updatedContact) => {
-            setContact(updatedContact);
-            setEditing(false);
-          }}
-        />
-      ) : (
         <>
-          <pre>{JSON.stringify(contact, null, 2)}</pre>
-          <button onClick={() => setEditing(true)}>Edit</button>
+          <h1>Editing Contact</h1>
+          <ContactForm
+            contact={contact}
+            onSubmit={(updatedContact) => {
+              setContact(updatedContact);
+              setEditing(false);
+            }}
+          />
         </>
+      ) : (
+        <ContactCard>
+          <EditButton onClick={() => setEditing(true)}>
+            <Icon type="edit" />
+          </EditButton>
+          <StyledFavoriteContact
+            contactId={contact.id}
+            isFavorite={contact.favorite}
+          />
+          <StyledContactAvatar email={contact.email} size="120" />
+          <ContactName>
+            {contact.firstName} {contact.lastName}
+          </ContactName>
+          <div>
+            <small>{contact.email}</small>
+            <small>{contact.phoneNumber}</small>
+          </div>
+        </ContactCard>
       )}
-    </div>
+    </>
   );
 }
 Contact.propTypes = {
