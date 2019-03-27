0fb05e4e5db5af2886631396ad6d46e62eccd385
Matt Jacobs <hello@mattjdev.com>
2019-03-27 11:44:29 -0500

    Add nprogress

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,6 +1,15 @@
 import App from 'next/app';
 import Modal from 'react-modal';
+import NProgress from 'nprogress';
+import Router from 'next/router';
 import Page from '../components/Page';
+
+NProgress.configure({ showSpinner: false });
+Router.events.on('routeChangeStart', () => {
+  NProgress.start();
+});
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 Modal.setAppElement('#__next');
 
