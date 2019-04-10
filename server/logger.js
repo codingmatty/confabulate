f14ae0022664b476ebb5230a198f87633598fc84
Matt Jacobs <hello@mattjdev.com>
2019-04-10 10:31:44 -0500

    Fix logger in dev mode

--- server/logger.js
+++ server/logger.js
@@ -3,17 +3,21 @@ const { LoggingWinston } = require('@google-cloud/logging-winston');
 
 // Create a Winston logger that streams to Stackdriver Logging
 // Logs will be written to: "projects/YOUR_PROJECT_ID/logs/winston_log"
+const transports = [
+  new winston.transports.Console({
+    colorize: true,
+    format: winston.format.simple(),
+    json: false,
+    level: 'debug'
+  })
+];
+if (process.env.NODE_ENV === 'production') {
+  transports.push(new LoggingWinston());
+}
+
 const logger = winston.createLogger({
   level: 'info',
-  transports: [
-    new winston.transports.Console({
-      colorize: true,
-      format: winston.format.simple(),
-      json: false,
-      level: 'debug'
-    }),
-    new LoggingWinston()
-  ]
+  transports
 });
 
 logger.stream = {
