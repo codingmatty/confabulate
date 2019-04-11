b57d20b0b48f5105731c000cb8d1621ecc04d6f1
Matt Jacobs <hello@mattjdev.com>
2019-04-11 12:11:55 -0500

    Update credential variables

--- server/firebase-admin.js
+++ server/firebase-admin.js
@@ -4,9 +4,11 @@ admin.initializeApp({
   projectId: process.env.FIREBASE_PROJECTID,
   authDomain: process.env.FIREBASE_AUTHDOMAIN,
   databaseURL: process.env.FIREBASE_DATABASEURL,
-  credential: admin.credential.cert(
-    JSON.parse(process.env.FIREBASE_CREDENTIALS_STRING)
-  )
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECTID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+  })
 });
 
 module.exports = admin;
