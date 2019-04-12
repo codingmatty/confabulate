f2a2cfd50c670fbdcf851f4b6fbbca52e8a37639
Matt Jacobs <hello@mattjdev.com>
2019-04-12 07:45:58 -0500

    Add cron executioner

---
+++ scripts/execute-cron.js
@@ -0,0 +1,35 @@
+/* global require Promise fetch */
+require('isomorphic-fetch');
+require('../server/setup-env-vars');
+const logger = require('../server/logger');
+
+const typeToJobMap = {
+  daily: ['daily-birthdays']
+};
+
+const type = process.argv[2];
+if (type) {
+  runCron(type);
+} else {
+  logger.info('Please provide a cron type');
+}
+
+function runCron(type) {
+  const jobsToRun = typeToJobMap[type] || [];
+  Promise.all(
+    jobsToRun.map(async (job) => {
+      const response = await fetch(
+        `https://app.confabulate.co/cron/${job}?secret=${
+          process.env.CRON_SECRET
+        }`
+      );
+      if (response.ok && response.status === 200) {
+        logger.info(`Cron job complete: ${job}`);
+      } else {
+        logger.info(`Cron job failed: ${job}`);
+      }
+    })
+  ).then(() => {
+    logger.info('All cron jobs complete');
+  });
+}
