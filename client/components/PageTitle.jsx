0f5e98374f34182dfe675509dea6a50c14936b44
Matt Jacobs <hello@mattjdev.com>
2019-04-14 22:25:12 -0500

    Create UtilityText and Button Component

--- client/components/PageTitle.jsx
+++ client/components/PageTitle.jsx
@@ -1,12 +1,5 @@
-import styled from 'styled-components';
-
-const StyledPageTitle = styled.h1`
-  color: ${({ theme }) => theme.color.neutrals[5]};
-  font-size: 1rem;
-  margin: 0 0 1rem;
-  text-transform: uppercase;
-`;
+import UtilityText from './styled/UtilityText';
 
 export default function PageTitle({ children }) {
-  return <StyledPageTitle>{children}</StyledPageTitle>;
+  return <UtilityText as="h1">{children}</UtilityText>;
 }
