7aa617b792f843d15f49ae9108e88f2e8235ac7d
Matt Jacobs <hello@mattjdev.com>
2019-04-17 21:02:48 -0500

    Add favicons and 'add to home' screen prompt

--- client/components/Head.jsx
+++ client/components/Head.jsx
@@ -5,12 +5,25 @@ export default function AppHead() {
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta charSet="utf-8" />
-      <link rel="shortcut icon" href="/static/favicon.png" />
+      <meta name="theme-color" content="#f7d070" />
+      <link rel="manifest" href="/static/site.webmanifest" />
+      <link rel="shortcut icon" href="/static/favicon.ico" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/static/apple-touch-icon.png"
+      />
+      <meta name="apple-mobile-web-app-title" content="Confabulate" />
       <link rel="stylesheet" type="text/css" href="/static/nprogress.css" />
       <link
         rel="stylesheet"
         type="text/css"
         href="/static/react-toastify.css"
+      />
+      <link
+        rel="stylesheet"
+        type="text/css"
+        href="/static/pwa-install-prompt.css"
       />
       <link
         href="https://fonts.googleapis.com/icon?family=Material+Icons"
